385c34a6b90cb45b8d52ea6718a021a7eda5cbc7
Dhruv Patel <dhruvpatel1380@gmail.com>
2026-02-06 20:32:31 +0530

    Fix image path build errorr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from './Nav';
-import logo1 from "../../public/images/patel.png.png"
-
+import styled from "styled-components";
+import Nav from "./Nav";
+import logo1 from "../../public/images/patel.png";
 
 const MainHeader = styled.header`
 padding:0 4.8rem;
 height: 10rem;
-background-color: ${({ theme}) => theme.colors.bg};
+background-color: ${({ theme }) => theme.colors.bg};
 display: flex;
 justify-content: space-between;
 align-items: center;
@@ -22,26 +21,23 @@ z-index:999;
 
 .logo {
 height: 25px;
-}`; 
+}`;
 
 const Header = () => {
-    const logo = {
-  width: "22rem",
-  maxWidth: "100%",
-  height: "auto",
+  const logo = {
+    width: "22rem",
+    maxWidth: "100%",
+    height: "auto",
+  };
 
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img style={logo} src={logo1} alt="my logo img" />
+      </NavLink>
+      <Nav />
+    </MainHeader>
+  );
 };
 
-    return (
-        <MainHeader>
-            <NavLink to="/">
-            <img style={logo}  src={logo1} alt="my logo img"/>
-            </NavLink>
-            <Nav/>
-        </MainHeader>
-    )
-};
-
- 
-
-export default Header
+export default Header;
