ed07ed3afbbd7dfeb981b41d1c5b8e837d32e214
Dhruv Patel <dhruvpatel1380@gmail.com>
2026-02-06 20:37:49 +0530

    Fix image path build err

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Nav from "./Nav";
-import logo1 from "../../public/images/patel.png";
+// import logo1 from "../../public/images/patel.png";
 
 const MainHeader = styled.header`
 padding:0 4.8rem;
